effa96508b6f91fee5a1f1c2556ae04889f57ca7
Netra <netrahindocha1@gmail.com>
2024-06-14 10:44:39 -0400

    Second commit Containing the completed Product class

---
+++ ECOM_PRODUCT/Product.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECOM_PRODUCT
+{
+    internal class Product
+    {
+        // Defining Properties
+        public int ProductId { get; set; }
+
+        public string ProductName { get; set; }
+
+        public double Price { get; set; }
+
+        public int Stock { get; set; }
+
+        // Parameterized Constructor with properties
+        public Product(int productId, string productName, double price, int stock)
+        {
+            ProductId = productId;
+            ProductName = productName;
+            Price = price;
+            Stock = stock;
+        }
+
+        // Method to Increase the Stock
+        public void StockIncrease(int numOfProducts)
+        {
+            if (numOfProducts > 0)
+            {
+                Stock += numOfProducts;
+            }
+            else
+            {
+                Stock += 0;
+            }
+        }
+
+        // Method to Decrease the Stock
+        public void StockDecrease(int numOfProducts)
+        {
+            if (numOfProducts > 0 && numOfProducts <= Stock)
+            {
+                Stock -= numOfProducts;
+            }
+            else 
+            {
+                Stock -= 0;
+            }
+        }
+    }
+}
